9ab2589d09631d28202d76cbd0ec55bab159fee5
NkosiDlamini <nkosingiphile.dlamini@capaciti.org.za>
2026-08-21 09:00:43 +0200

    Add click event to buttons with alert message

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Get all assistant buttons on the page
+const buttons = document.querySelectorAll("button");
 
+// Add an event listener to each button
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        alert("This feature is coming soon!");
+    });
+});
